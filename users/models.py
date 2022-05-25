954dd8dec2ad9fc50a0d077e9f6231cde7d043d9
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2022-05-25 09:35:30 +0200

    Fuera linkedin

--- users/models.py
+++ users/models.py
@@ -3,9 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    pass
+    """
     email = models.EmailField(unique=True)
 
     linkedin = models.CharField(max_length=255, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    """
